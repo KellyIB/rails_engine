04f7fc9d1985f3a6a6a710afbe2b1fba131d02fb
KellyIB <kellyibard@gmail.com>
2020-03-17 17:55:11 -0600

    create model, testing and endpoints for items, create items table

---
+++ spec/factories/items.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :item do
+    name { Faker::Commerce.product_name }
+    description { Faker::ChuckNorris.fact }
+    unit_price { Faker::Commerce.price }
+  end
+end
