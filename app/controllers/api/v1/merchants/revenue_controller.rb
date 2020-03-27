0acc1e56df48080156979c7c1724858463019100
KellyIB <kellyibard@gmail.com>
2020-03-27 13:00:30 -0600

    add render json and serializer to most_revenue in revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def most_revenue
-    Merchant.most_revenue(params[:quantity])
+    render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
     # Merchant.joins(:transactions).joins(:invoice_items).where("transactions.result = '0'").select('merchants.*, sum(invoice_items.unit_price * invoice_items.quantity) as revenue').group(:id).order(revenue: :desc).limit("#{params[:quantity]}")
   end
 
